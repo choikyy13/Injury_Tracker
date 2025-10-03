c2a690caa9efbe25531f01b611d0cfa4441b7919
Yiheng <ycao@>
2025-10-03 13:53:30 +0200

    Add Users Table, Change most Primary key

--- Assignment 2/Injury_Tracker.sql
+++ Assignment 2/Injury_Tracker.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Athlete (
-    Athlete_id INT,
+    Athlete_id CHAR(5) PRIMARY KEY,  -- A0001
     Athlete_name CHAR(30) NOT NULL,
     ssn INT,
     Date_of_birth DATE,
@@ -9,20 +9,20 @@ CREATE TABLE Athlete (
 );
 
 CREATE TABLE Team (
-    Team_id INT PRIMARY KEY,
+    Team_id CHAR(5) PRIMARY KEY,  -- T0001
     Team_name CHAR(30),
     Country CHAR(30),
 );
 
 
 CREATE TABLE Injury_Type (
-    Injury_id INT PRIMARY KEY,
+    Injury_id CHAR(7) PRIMARY KEY,  -- Inj0001
     Injury_name CHAR(50),
     Body_part CHAR(30),
 );
 
 CREATE TABLE Injury_Record (
-    Record_id INT PRIMARY KEY,
+    Record_id CHAR(7) PRIMARY KEY,  -- Rec0001
     Athlete_id INT NOT NULL,
     Injury_id INT NOT NULL,
     Date_inj DATE,
@@ -34,35 +34,49 @@ CREATE TABLE Injury_Record (
 );
 
 CREATE TABLE Short_Term (
-    Record_id INT PRIMARY KEY,
+    Record_id CHAR(7) PRIMARY KEY, 
     Treatment CHAR(30),
     Recovery_time CHAR(30),
     FOREIGN KEY (Record_id) REFERENCES Injury_Record (Record_id)
 );
 
 CREATE TABLE Long_Term (
-    Record_id INT PRIMARY KEY,
+    Record_id CHAR(7) PRIMARY KEY,
     Long_term_effect CHAR(100),
     Able_to_continue_career bool,
     FOREIGN KEY(Record_id) REFERENCES Injury_Record(Record_id)
 );
 
+CREATE TABLE Users(
+    User_id int PRIMARY KEY,
+    User_name CHAR(30),
+    Email CHAR(30) UNIQUE
+);
+
 CREATE TABLE Public (
     User_id INT PRIMARY KEY,
     Interest CHAR(100),
-    Staff_id INT,
-    FOREIGN KEY (Staff_id) REFERENCES Staff(Staff_id),
+    Team_supporting INT,
+    FOREIGN KEY (Team_supporting) REFERENCES Team(Team_id),
     FOREIGN KEY (User_id) REFERENCES User(User_id)
 );
 
+CREATE TABLE Professional (
+    User_id int PRIMARY KEY,
+    Permission_code int unique,
+    Staff_id CHAR(5),
+    FOREIGN KEY (Staff_id) REFERENCES Staff(Staff_id),
+    FOREIGN KEY (User_id) REFERENCES Users(User_id)
+);
+
 CREATE TABLE Staff (
-    Staff_id INT PRIMARY KEY,
+    Staff_id CHAR(5) PRIMARY KEY,  -- S0001
     Staff_name CHAR(30),
     Year_join INT
 );
 
 CREATE TABLE Coach (
-    Staff_id INT PRIMARY KEY,
+    Staff_id CHAR(5) PRIMARY KEY,  -- C0001
     Team_id INT,
     Certificate CHAR(30),
     FOREIGN KEY (Team_id) REFERENCES Team(Team_id),
@@ -70,7 +84,7 @@ CREATE TABLE Coach (
 );
 
 CREATE TABLE Medical_Team (
-    Staff_id INT PRIMARY KEY,
+    Staff_id CHAR(7) PRIMARY KEY, -- Med0001
     Specialization CHAR(40),
     License_num CHAR(30),
     FOREIGN KEY(Staff_id) REFERENCES Staff(Staff_id)
