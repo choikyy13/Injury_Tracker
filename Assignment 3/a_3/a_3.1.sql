5de7e5783dd84c248d0014fffa9de77dc5c59314
wasi0509 <tinkerbellsin@gmail.com>
2025-10-08 01:29:59 +0800

    change staff name

--- Assignment 3/a_3/a_3.1.sql
+++ Assignment 3/a_3/a_3.1.sql
@@ -1,6 +1,6 @@
--- list out the athletes' informations which are in charge of Dr. Noah Smith and not recovered yet
+-- list out the athletes' informations which are in charge of Noah Smith and not recovered yet
 select a.*
 from Athlete a
 join Injury_record i ON a.Athlete_id = i.Athlete_id
-where i.STAFF_in_Charge = (select Staff_id from Staff where Staff_name = "Dr. Noah Smith")
+where i.STAFF_in_Charge = (select Staff_id from Staff where Staff_name = "Noah Smith")
 		AND i.Injury_Status != "Recovered"
