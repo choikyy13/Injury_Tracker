c54872a39c651a0588c8f1b3c40028c5acee9c20
wasi0509 <tinkerbellsin@gmail.com>
2025-10-09 22:23:25 +0800

    final

--- Assignment 3/a_3/a_3.5.sql
+++ Assignment 3/a_3/a_3.5.sql
@@ -3,6 +3,3 @@ SELECT COUNT(*) AS Public_User_Count
 FROM Users u 
 JOIN Public p
     ON p.User_id = u.User_id;
-
-
-
--- Assignment 3/a_3/a_3.6.sql
+++ Assignment 3/a_3/a_3.6.sql
@@ -1,4 +1,5 @@
 -- create a list of coaches name and their certificate who has no associated team
+
 SELECT s.Staff_name, c.Cerificate
 FROM Staff s, Coach c 
-WHERE s.Staff_id = c.Staff_id AND C.Team_id IS NULL;
+WHERE s.Staff_id = c.Staff_id AND c.Team_id IS NULL; 
